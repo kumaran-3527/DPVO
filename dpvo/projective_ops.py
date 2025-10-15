4c46641d2751575e3ad7ab3e3f3ab1a847ab4183
kumaran-3527 <kumaransk02@gmail.com>
2025-10-15 14:20:56 +0530

    added missing function

--- dpvo/projective_ops.py
+++ dpvo/projective_ops.py
@@ -141,6 +141,187 @@ def induced_flow(poses, disps, intrinsics, ii, jj):
     )
     
     coords0 = torch.stack([x, y], dim=-1)
-    coords1, valid = transform(poses, disps, intrinsics, ii, jj, False)
+    coords1, valid = projective_transform(poses, disps, intrinsics, ii, jj, False)
 
     return coords1[..., :2] - coords0, valid
+
+
+
+
+def projective_transform(
+    poses, depths, intrinsics, ii, jj, jacobian=False, return_depth=False
+):
+    """map points from ii->jj"""
+
+    # inverse project (pinhole)
+    X0, Jz = iproj_1(depths[:, ii], intrinsics[:, ii], jacobian=jacobian)
+
+    # transform
+    Gij = poses[:, jj] * poses[:, ii].inv()
+
+    Gij.data[:, ii == jj] = torch.as_tensor(
+        [-0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0], device="cuda"
+    )
+    X1, Ja = actp(Gij, X0, jacobian=jacobian)
+
+    # project (pinhole)
+    x1, Jp = proj_1(X1, intrinsics[:, jj], jacobian=jacobian, return_depth=return_depth)
+
+    # exclude points too close to camera
+    valid = ((X1[..., 2] > MIN_DEPTH) & (X0[..., 2] > MIN_DEPTH)).float()
+    valid = valid.unsqueeze(-1)
+
+    if jacobian:
+        # Ji transforms according to dual adjoint
+        Jj = torch.matmul(Jp, Ja)
+        Ji = -Gij[:, :, None, None, None].adjT(Jj)
+
+        Jz = Gij[:, :, None, None] * Jz
+        Jz = torch.matmul(Jp, Jz.unsqueeze(-1))
+
+        return x1, valid, (Ji, Jj, Jz)
+
+    return x1, valid
+
+
+def actp(Gij, X0, jacobian=False):
+    """action on point cloud"""
+    X1 = Gij[:, :, None, None] * X0
+
+    if jacobian:
+        X, Y, Z, d = X1.unbind(dim=-1)
+        o = torch.zeros_like(d)
+        B, N, H, W = d.shape
+
+        if isinstance(Gij, SE3):
+            Ja = torch.stack(
+                [
+                    d,
+                    o,
+                    o,
+                    o,
+                    Z,
+                    -Y,
+                    o,
+                    d,
+                    o,
+                    -Z,
+                    o,
+                    X,
+                    o,
+                    o,
+                    d,
+                    Y,
+                    -X,
+                    o,
+                    o,
+                    o,
+                    o,
+                    o,
+                    o,
+                    o,
+                ],
+                dim=-1,
+            ).view(B, N, H, W, 4, 6)
+
+        elif isinstance(Gij, Sim3):
+            Ja = torch.stack(
+                [
+                    d,
+                    o,
+                    o,
+                    o,
+                    Z,
+                    -Y,
+                    X,
+                    o,
+                    d,
+                    o,
+                    -Z,
+                    o,
+                    X,
+                    Y,
+                    o,
+                    o,
+                    d,
+                    Y,
+                    -X,
+                    o,
+                    Z,
+                    o,
+                    o,
+                    o,
+                    o,
+                    o,
+                    o,
+                    o,
+                ],
+                dim=-1,
+            ).view(B, N, H, W, 4, 7)
+
+        return X1, Ja
+
+    return X1, None
+
+
+def iproj_1(disps, intrinsics, jacobian=False):
+    """pinhole camera inverse projection"""
+    ht, wd = disps.shape[2:]
+    fx, fy, cx, cy = extract_intrinsics(intrinsics)
+
+    y, x = torch.meshgrid(
+        torch.arange(ht, device=disps.device, dtype=torch.float),
+        torch.arange(wd, device=disps.device, dtype=torch.float),
+        indexing="ij",
+    )
+
+    i = torch.ones_like(disps)
+    X = (x - cx) / fx
+    Y = (y - cy) / fy
+    pts = torch.stack([X, Y, i, disps], dim=-1)
+
+    if jacobian:
+        J = torch.zeros_like(pts)
+        J[..., -1] = 1.0
+        return pts, J
+
+    return pts, None
+
+
+
+def proj_1(Xs, intrinsics, jacobian=False, return_depth=False):
+    """pinhole camera projection"""
+    fx, fy, cx, cy = extract_intrinsics(intrinsics)
+    X, Y, Z, D = Xs.unbind(dim=-1)
+
+    Z = torch.where(Z < 0.5 * MIN_DEPTH, torch.ones_like(Z), Z)
+    d = 1.0 / Z
+
+    x = fx * (X * d) + cx
+    y = fy * (Y * d) + cy
+    if return_depth:
+        coords = torch.stack([x, y, D * d], dim=-1)
+    else:
+        coords = torch.stack([x, y], dim=-1)
+
+    if jacobian:
+        B, N, H, W = d.shape
+        o = torch.zeros_like(d)
+        proj_jac = torch.stack(
+            [
+                fx * d,
+                o,
+                -fx * X * d * d,
+                o,
+                o,
+                fy * d,
+                -fy * Y * d * d,
+                o,
+                # o,     o,    -D*d*d,  d,
+            ],
+            dim=-1,
+        ).view(B, N, H, W, 2, 4)
+
+        return coords, proj_jac
+
+    return coords, None
